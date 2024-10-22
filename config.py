123bf2020f08511102dc5c9f5fe516998951f271
sitaratoon <ajay393798@gmail.com>
2024-10-22 13:33:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,10 +27,10 @@ API_HASH = environ.get("API_HASH", "c163d4e28e63196c3806cf3b9b2885de")
 ADMINS = int(environ.get("ADMINS", "6692613520"))
 
 # Database Information
-CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://ajay393798:ijwrgGv5P4sIP54h@cluster0.eqraqbm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://sitaratoons98:Ajay12398@cluster0.6ptzu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 CDB_NAME = environ.get("CDB_NAME", "clonevjbotz")
-DB_URI = environ.get("DB_URI", "mongodb+srv://ajay393798:btFPigbsUQK3MYt9@cluster0.ylenz.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = environ.get("DB_NAME", "vjbotz")
+DB_URI = environ.get("DB_URI", "mongodb+srv://ajay393798:btFPigbsUQK3MYt9@cluster0.ylenz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = environ.get("DB_NAME", "sitaratoons")
 
 
 # Bot Information
